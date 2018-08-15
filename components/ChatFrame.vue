ed418e91538f3195d099bfb000252349c0c72688
Remi Remino <remi@remino.net>
2018-08-15 11:19:28 +0900

    Reword greeting

--- components/ChatFrame.vue
+++ components/ChatFrame.vue
@@ -58,7 +58,19 @@ export default
     messages: [
       {
         id: 1
-        content: 'Hello'
+        content: 'nice to meet you'
+        loaded: true
+        sender: 'bot'
+      }
+      {
+        id: 2
+        content: 'my name is TAKESHITAROU'
+        loaded: true
+        sender: 'bot'
+      }
+      {
+        id: 3
+        content: 'where from?'
         loaded: true
         sender: 'bot'
       }
